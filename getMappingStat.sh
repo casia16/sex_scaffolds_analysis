b500601bdd40713d4162f27745caabc59dedd128
casia16 <casia16@gmail.com>
2020-11-26 16:45:44 +0100

    Update getMappingStat.sh

--- getMappingStat.sh
+++ getMappingStat.sh
@@ -5,6 +5,6 @@ while read -r sample_path <&3; do
 s=${sample_path%.bam}
 sample=${s##*/}
 echo $sample
-samtools idxstats $sample_path > $output_dir/idxstat_for_sex_scaffolds/$sample.idxstat
+samtools idxstats $sample_path > $output_dir/idxstats_for_sex_scaffolds/$sample.idxstat
 done 3< <(ls  $input_dir/*.bam)
 
